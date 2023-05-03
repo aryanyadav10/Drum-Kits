9c97cc9cc0f96210f0a56686d7fd56b7beec98a8
Aryan Yadav <yadavaryan2003@gmail.com>
2023-05-03 19:42:39 +0530

    Commit1

---
+++ main.js
@@ -0,0 +1,26 @@
+const kits = ["crash","kick","snare","tom"];
+
+const containerEl = document.querySelector(".container");
+
+kits.forEach(kits=> {
+    const btnEl = document.createElement("button");
+    btnEl.classList.add("btn");
+    btnEl.innerText = kits;
+    btnEl.style.backgroundImage = "url(images/"+kits+".png)";
+    containerEl.appendChild(btnEl);
+    const audioEl = document.createElement("audio");
+    containerEl.appendChild(audioEl);
+    audioEl.src = "Sounds/"+kits+".mp3"
+    btnEl.addEventListener("click",(event)=> {
+        audioEl.play();
+    });
+    window.addEventListener("keydown",(event)=> {
+        if(event.key === kits.slice(0,1)){
+            audioEl.play();
+            btnEl.style.transform = "scale(.9)";
+            setTimeout(()=> {
+               btnEl.style.transform = "scale(1)";
+            },100);
+        };
+    })
+});
